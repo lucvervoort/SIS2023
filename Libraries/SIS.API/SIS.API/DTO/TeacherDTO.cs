b4b3bedf2d47cd3d4049140a31bb066e173577f9
Luc Vervoort <luc.vervoort@hogent.be>
2023-10-23 17:32:04 +0200

    Example of annotation in DTO

--- Libraries/SIS.API/SIS.API/DTO/TeacherDTO.cs
+++ Libraries/SIS.API/SIS.API/DTO/TeacherDTO.cs
@@ -1,7 +1,10 @@
-﻿namespace SIS.API.DTO
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SIS.API.DTO
 {
     public class TeacherDTO
     {
+        [MaxLength(50)]
         public string FirstName { get; set; } = string.Empty;
         public string LastName { get; set; } = string.Empty;
     }
