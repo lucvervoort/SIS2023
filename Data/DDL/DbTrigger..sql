696e0c7e694d07ae7c60eeb612271320144ad609
Luc Vervoort <luc.vervoort@hogent.be>
2023-10-24 22:35:45 +0200

    Update triggers

--- Data/DDL/DbTrigger..sql
+++ Data/DDL/DbTrigger..sql
@@ -1,4 +1,6 @@
-﻿IF EXISTS (select * from sysobjects where name like '%TG_AcademicYear_Update%')
+﻿-- TODO: check if all tables have triggers and if all triggers work
+
+IF EXISTS (select * from sysobjects where name like '%TG_AcademicYear_Update%')
 DROP TRIGGER TG_AcademicYear_Update
 GO
 Create trigger TG_AcademicYear_Update
@@ -109,11 +111,11 @@ as
 begin
 if update(ContactId) or update(PersonId) or update(CompanyId) or update(Verified) or update(FunctionRole) or update(Department) or exists (select * from deleted)
 BEGIN
-    UPDATE c
-        set c.AUTO_UPDATE_COUNT = c.AUTO_UPDATE_COUNT + 1, c.AUTO_TIME_UPDATE = GETDATE()
-    FROM deleted d
-        INNER JOIN Contact c
-        ON c.ContactId = d.AcademicYearId
+   UPDATE c
+       set c.AUTO_UPDATE_COUNT = c.AUTO_UPDATE_COUNT + 1, c.AUTO_TIME_UPDATE = GETDATE()
+   FROM deleted d
+       INNER JOIN Contact c
+       ON c.ContactId = d.ContactId
 END
 end
 GO
@@ -154,24 +156,6 @@ BEGIN
 		ON a.LocationId = d.LocationId
 END 
 end
-
-IF EXISTS (select * from sysobjects where name like '%TG_Building_Update%')
-DROP TRIGGER TG_Building_Update
-GO
-Create trigger TG_Building_Update
-on Building after insert, update
-as
-begin
-if update(BuildingId) or update(Name) or update(LocationId) or update(IsDeleted) or exists (select * from deleted)
-BEGIN
-	UPDATE a
-		set a.AUTO_UPDATE_COUNT = a.AUTO_UPDATE_COUNT + 1, a.AUTO_TIME_UPDATE = GETDATE()
-	FROM deleted d
-		INNER JOIN Building a
-		ON a.BuildingId = d.BuildingId
-END 
-end
-GO
 
 IF exists (select * from sysobjects where name like '%TG_RoomType_Update%')
 DROP TRIGGER TG_RoomType_Update
@@ -227,25 +211,25 @@ BEGIN
 END 
 end
 
--- DenderValley
-
-IF EXISTS (select * from sysobjects where name like '%TG_Person%')
-DROP TRIGGER TG_Person_Update
+IF EXISTS (select * from sysobjects where name like '%TG_Building_Update%')
+DROP TRIGGER TG_Building_Update
 GO
-Create trigger TG_Person_Update
-on Person after insert, update
+Create trigger TG_Building_Update
+on Building after insert, update
 as
 begin
-if update(PersonID) or update(FirstName) or update(LastName) or update(SortName) or update(Phone) or update(Mobile) or update(Email) or update(AdressId) or  update(IsDeleted) or exists (select * from deleted)
+if update(BuildingId) or update(Name) or update(LocationId) or update(IsDeleted) or exists (select * from deleted)
 BEGIN
-    UPDATE a
-        set a.AUTO_UPDATE_COUNT = a.AUTO_UPDATE_COUNT + 1, a.AUTO_TIME_UPDATE = GETDATE()
-    FROM deleted d
-        INNER JOIN Person a
-        ON a.PersonID = d.PersonID
+	UPDATE a
+		set a.AUTO_UPDATE_COUNT = a.AUTO_UPDATE_COUNT + 1, a.AUTO_TIME_UPDATE = GETDATE()
+	FROM deleted d
+		INNER JOIN Building a
+		ON a.BuildingId = d.BuildingId
 END 
 end
 GO
+
+-- DenderValley
 
 IF EXISTS (select * from sysobjects where name like '%TG_RegistrationState%')
 DROP TRIGGER TG_RegistrationState_Update
@@ -302,20 +286,21 @@ END
 end
 GO
 
-IF EXISTS (select * from sysobjects where name like '%TG_StudentIEOM%')
-DROP TRIGGER TG_StudentIEOM_Update
+-- naming was wrong:
+IF EXISTS (select * from sysobjects where name like '%TG_Student_IOEM%')
+DROP TRIGGER TG_StudentIOEM_Update
 GO
-Create trigger TG_StudentIEOM_Update
-on StudentIEOM after insert, update
+Create trigger TG_StudentIOEM_Update
+on StudentIOEM after insert, update
 as
 begin
-if update(StudentIEOMId) or update(AcademicYearId) or update(StudentId) or update(IOEMId) or update(IsDeleted) or exists (select * from deleted)
+if update(StudentIOEMId) or update(AcademicYearId) or update(StudentId) or update(IOEMId) or update(IsDeleted) or exists (select * from deleted)
 BEGIN
     UPDATE a
         set a.AUTO_UPDATE_COUNT = a.AUTO_UPDATE_COUNT + 1, a.AUTO_TIME_UPDATE = GETDATE()
     FROM deleted d
-        INNER JOIN StudentIEOM a
-        ON a.StudentIEOMId = d.StudentIEOMId
+        INNER JOIN StudentIOEM a
+        ON a.StudentIOEMId = d.StudentIOEMId
 END 
 end
 GO
@@ -334,25 +319,6 @@ BEGIN
     FROM deleted d
         INNER JOIN TeacherType a
         ON a.TeacherTypeId = d.TeacherTypeId
-END 
-end
-GO
-
-
-IF EXISTS (select * from sysobjects where name like '%TG_Teacher%')
-DROP TRIGGER TG_Teacher_Update
-GO
-Create trigger TG_Teacher_Update
-on Teacher after insert, update
-as
-begin
-if update(TeacherId) or update(Abbreviation) or update(Mobile) or update(Email) or update(AssignmentPercentage) or update(TeacherTypeId) or update(RegistrationStateId) or update(PersonId) or update(IsDeleted) or exists (select * from deleted)
-BEGIN
-    UPDATE a
-        set a.AUTO_UPDATE_COUNT = a.AUTO_UPDATE_COUNT + 1, a.AUTO_TIME_UPDATE = GETDATE()
-    FROM deleted d
-        INNER JOIN Teacher a
-        ON a.TeacherId = d.TeacherId
 END 
 end
 GO
@@ -425,6 +391,43 @@ BEGIN
     FROM deleted d
         INNER JOIN Teacher_TeacherGroup a
         ON a.Teacher_TeacherGroupId = d.Teacher_TeacherGroupId
+END 
+end
+GO
+
+
+IF EXISTS (select * from sysobjects where name like '%TG_Teacher_Update%')
+DROP TRIGGER TG_Teacher_Update
+GO
+Create trigger TG_Teacher_Update
+on Teacher after insert, update
+as
+begin
+if update(TeacherId) or update(Abbreviation) or update(Mobile) or update(Email) or update(AssignmentPercentage) or update(TeacherTypeId) or update(RegistrationStateId) or update(PersonId) or update(IsDeleted) or exists (select * from deleted)
+BEGIN
+    UPDATE a
+        set a.AUTO_UPDATE_COUNT = a.AUTO_UPDATE_COUNT + 1, a.AUTO_TIME_UPDATE = GETDATE()
+    FROM deleted d
+        INNER JOIN Teacher a
+        ON a.TeacherId = d.TeacherId
+END 
+end
+GO
+
+IF EXISTS (select * from sysobjects where name like '%TG_Person%')
+DROP TRIGGER TG_Person_Update
+GO
+Create trigger TG_Person_Update
+on Person after insert, update
+as
+begin
+if update(PersonID) or update(FirstName) or update(LastName) or update(SortName) or update(Phone) or update(Mobile) or update(Email) or update(AddressId) or update(IsDeleted) or exists (select * from deleted)
+BEGIN
+    UPDATE a
+        set a.AUTO_UPDATE_COUNT = a.AUTO_UPDATE_COUNT + 1, a.AUTO_TIME_UPDATE = GETDATE()
+    FROM deleted d
+        INNER JOIN Person a
+        ON a.PersonID = d.PersonID
 END 
 end
 GO
@@ -510,7 +513,7 @@ Create trigger TG_TeacherPreference_Update
 on TeacherPreference after insert, update
 as
 begin
-    if update(TeacherPreferenceId) or update(Name) or update(Preference) or update(Description) or exists (select *
+    if update(TeacherPreferenceId) or update(Preference) or update(Description) or exists (select *
         from deleted)
 BEGIN
         UPDATE tp
@@ -532,11 +535,11 @@ Create trigger TG_TeacherCoordinationRoleInterest_Update
 on TeacherCoordinationRoleInterest after insert, update
 as
 begin
-    if update(TeacherCoordinationRoleInterestId) or update(Name) or update(AssignmentPercentage) or exists (select *
+    if update(TeacherCoordinationRoleInterestId) or exists (select *
         from deleted)
 BEGIN
         UPDATE tc
-        set a.AUTO_UPDATE_COUNT = tc.AUTO_UPDATE_COUNT + 1, tc.AUTO_TIME_UPDATE = GETDATE()
+        set tc.AUTO_UPDATE_COUNT = tc.AUTO_UPDATE_COUNT + 1, tc.AUTO_TIME_UPDATE = GETDATE()
     FROM deleted d
             INNER JOIN TeacherCoordinationRoleInterest tc
             ON tc.TeacherCoordinationRoleInterestId = d.TeacherCoordinationRoleInterestId
@@ -666,11 +669,11 @@ begin
     if update(TeacherAssignmentPercentageInterestId) or update(AcademicYearId) or update(TeacherId) or update(PeriodId) or update(Percentage) or exists (select *
         from deleted)
 BEGIN
-        UPDATE tap
-        set tapi.AUTO_UPDATE_COUNT = tap.AUTO_UPDATE_COUNT + 1, tap.AUTO_TIME_UPDATE = GETDATE()
+        UPDATE tapi
+        set tapi.AUTO_UPDATE_COUNT = tapi.AUTO_UPDATE_COUNT + 1, tapi.AUTO_TIME_UPDATE = GETDATE()
     FROM deleted d
             INNER JOIN TeacherAssignmentPercentageInterest tapi
-            ON tap.TeacherAssignmentPercentageInterestId = d.TeacherAssignmentPercentageInterestId
+            ON tapi.TeacherAssignmentPercentageInterestId = d.TeacherAssignmentPercentageInterestId
     END
 end
 GO
@@ -805,4 +808,274 @@ BEGIN
             ON s.ScheduleId = d.ScheduleId
     END
 end
+GO
+
+-- Jorn
+
+IF EXISTS (select * from sysobjects where name like '%TG_AcademicYear_Update%')
+DROP TRIGGER TG_AcademicYear_Update
+GO
+Create trigger TG_AcademicYear_Update
+on AcademicYear after insert, update
+as
+begin
+if update(AcademicYearId) or update(Description) or update(StartDate) or update(StopDate) or update(IsDeleted) or exists (select * from deleted)
+BEGIN
+    UPDATE a
+        set a.AUTO_UPDATE_COUNT = a.AUTO_UPDATE_COUNT + 1, a.AUTO_TIME_UPDATE = GETDATE()
+    FROM deleted d
+        INNER JOIN AcademicYear a
+        ON a.AcademicYearId = d.AcademicYearId
+END 
+end
+GO
+
+IF EXISTS (select * from sysobjects where name like '%TG_Trajectory_Update%')
+DROP TRIGGER TG_Trajectory_Update
+GO
+Create trigger TG_Trajectory_Update
+on Trajectory after insert, update
+as
+begin
+if update(TrajectoryId) or update(Name) or update(Description) or update(IsDeleted) or exists (select * from deleted)
+BEGIN
+    UPDATE t
+        set t.AUTO_UPDATE_COUNT = t.AUTO_UPDATE_COUNT + 1, t.AUTO_TIME_UPDATE = GETDATE()
+    FROM deleted d
+        INNER JOIN Trajectory t
+        ON t.TrajectoryId = d.TrajectoryId
+END 
+end
+GO
+
+IF EXISTS (select * from sysobjects where name like '%TG_CourseType_Update%')
+DROP TRIGGER TG_CourseType_Update
+GO
+Create trigger TG_CourseType_Update
+on CourseType after insert, update
+as
+begin
+if update(CourseTypeId) or update(Name) or update(IsDeleted) or exists (select * from deleted)
+BEGIN
+    UPDATE ct
+        set ct.AUTO_UPDATE_COUNT = ct.AUTO_UPDATE_COUNT + 1, ct.AUTO_TIME_UPDATE = GETDATE()
+    FROM deleted d
+        INNER JOIN CourseType ct
+        ON ct.CourseTypeId = d.CourseTypeId
+END 
+end
+GO
+
+IF EXISTS (select * from sysobjects where name like '%TG_Course_Update%')
+DROP TRIGGER TG_Course_Update
+GO
+Create trigger TG_Course_Update
+on Course after insert, update
+as
+begin
+if update(CourseId) or update(LetterId) or update(Name) or update(CourseTypeId) or update(Points) or update (Weight) or update(HoursTotal) or update (HoursContact) or exists (select * from deleted)
+BEGIN
+    UPDATE c
+        set c.AUTO_UPDATE_COUNT = c.AUTO_UPDATE_COUNT + 1, c.AUTO_TIME_UPDATE = GETDATE()
+    FROM deleted d
+        INNER JOIN Course c
+        ON c.CourseId = d.CourseId
+END 
+end
+GO
+
+IF EXISTS (select * from sysobjects where name like '%TG_CourseTrajectory_Update%')
+DROP TRIGGER TG_CourseTrajectory_Update
+GO
+Create trigger TG_CourseTrajectory_Update
+on CourseTrajectory after insert, update
+as
+begin
+if update(CourseTrajectoryId) or update(CourseId) or update(TrajectoryId) or update(StudyPoints) or update(StudySheetUrl) or exists (select * from deleted)
+BEGIN
+    UPDATE ctr
+        set ctr.AUTO_UPDATE_COUNT = ctr.AUTO_UPDATE_COUNT + 1, ctr.AUTO_TIME_UPDATE = GETDATE()
+    FROM deleted d
+        INNER JOIN CourseTrajectory ctr
+        ON ctr.CourseTrajectoryId = d.CourseTrajectoryId
+END 
+end
+GO
+
+IF EXISTS (select * from sysobjects where name like '%TG_TopicCategory_Update%')
+DROP TRIGGER TG_TopicCategory_Update
+GO
+Create trigger TG_TopicCategory_Update
+on TopicCategory after insert, update
+as
+begin
+if update(TopicCategoryId) or update(Description) or exists (select * from deleted)
+BEGIN
+    UPDATE tc
+        set tc.AUTO_UPDATE_COUNT = tc.AUTO_UPDATE_COUNT + 1, tc.AUTO_TIME_UPDATE = GETDATE()
+    FROM deleted d
+        INNER JOIN TopicCategory tc
+        ON tc.TopicCategoryId = d.TopicCategoryId
+END 
+end
+GO
+
+IF EXISTS (select * from sysobjects where name like '%TG_Topic_Update%')
+DROP TRIGGER TG_Topic_Update
+GO
+Create trigger TG_Topic_Update
+on Topic after insert, update
+as
+begin
+if update(TopicId) or update(TopicCategoryid) or update(Description) or update(Priority) or exists (select * from deleted)
+BEGIN
+    UPDATE tp
+        set tp.AUTO_UPDATE_COUNT = tp.AUTO_UPDATE_COUNT + 1, tp.AUTO_TIME_UPDATE = GETDATE()
+    FROM deleted d
+        INNER JOIN Topic tp
+        ON tp.TopicId = d.TopicId
+END 
+end
+GO
+
+IF EXISTS (select * from sysobjects where name like '%TG_LearningOutcomeType_Update%')
+DROP TRIGGER TG_LearningOutcomeType_Update
+GO
+Create trigger TG_LearningOutcomeType_Update
+on LearningOutcomeType after insert, update
+as
+begin
+if update(LearningOutcomeTypeId) or update(Name) or update(Description) or exists (select * from deleted)
+BEGIN
+    UPDATE lot
+        set lot.AUTO_UPDATE_COUNT = lot.AUTO_UPDATE_COUNT + 1, lot.AUTO_TIME_UPDATE = GETDATE()
+    FROM deleted d
+        INNER JOIN LearningOutcomeType lot
+        ON lot.LearningOutcomeTypeId = d.LearningOutcomeTypeId
+END 
+end
+GO
+
+IF EXISTS (select * from sysobjects where name like '%TG_LearningOutcome_Update%')
+DROP TRIGGER TG_LearningOutcome_Update
+GO
+Create trigger TG_LearningOutcome_Update
+on LearningOutcome after insert, update
+as
+begin
+if update(LearningOutcomeId) or update(Name) or update(Description) or update(LearningOutcomeTypeId) or exists (select * from deleted)
+BEGIN
+    UPDATE lo
+        set lo.AUTO_UPDATE_COUNT = lo.AUTO_UPDATE_COUNT + 1, lo.AUTO_TIME_UPDATE = GETDATE()
+    FROM deleted d
+        INNER JOIN LearningOutcome lo
+        ON lo.LearningOutcomeId = d.LearningOutcomeId
+END 
+end
+GO
+
+IF EXISTS (select * from sysobjects where name like '%TG_ControlLevel_Update%')
+DROP TRIGGER TG_ControlLevel_Update
+GO
+Create trigger TG_ControlLevel_Update
+on ControlLevel after insert, update
+as
+begin
+if update(ControlLevelId) or update(Name) or update(Abbreviation) or update(Description)
+BEGIN
+    UPDATE cl
+        set cl.AUTO_UPDATE_COUNT = cl.AUTO_UPDATE_COUNT + 1, cl.AUTO_TIME_UPDATE = GETDATE()
+    FROM deleted d
+        INNER JOIN ControlLevel cl
+        ON cl.ControlLevelId = d.ControlLevelId
+END 
+end
+GO
+
+IF EXISTS (select * from sysobjects where name like '%TG_LearningTarget_Update%')
+DROP TRIGGER TG_LearningTarget_Update
+GO
+Create trigger TG_LearningTarget_Update
+on LearningTarget after insert, update
+as
+begin
+if update(LearningTargetId) or update(Name) or update(Description) or update(LearningOutcomeId) or update(ControlLevelId) or exists (select * from deleted)
+BEGIN
+    UPDATE lt
+        set lt.AUTO_UPDATE_COUNT = lt.AUTO_UPDATE_COUNT + 1, lt.AUTO_TIME_UPDATE = GETDATE()
+    FROM deleted d
+        INNER JOIN LearningTarget lt
+        ON lt.LearningTargetId = d.LearningTargetId
+END 
+end
+GO
+
+-- Glen en M
+
+-- InfoType table:
+
+IF EXISTS (SELECT * FROM sysobjects WHERE name = 'TG_InfoType_Update' AND xtype = 'TR')
+    DROP TRIGGER TG_InfoType_Update
+GO
+
+CREATE TRIGGER TG_InfoType_Update
+ON InfoType AFTER INSERT, UPDATE
+AS
+BEGIN
+    IF UPDATE(Name) OR UPDATE(AUTO_TIME_CREATION) OR UPDATE(AUTO_TIME_UPDATE) OR UPDATE(AUTO_UPDATE_COUNT) OR UPDATE(IsDeleted) OR EXISTS (SELECT * FROM deleted)
+    BEGIN
+        UPDATE i
+        SET i.AUTO_UPDATE_COUNT = i.AUTO_UPDATE_COUNT + 1, i.AUTO_TIME_UPDATE = GETDATE()
+        FROM deleted d
+        INNER JOIN InfoType i
+        ON i.InfoTypeId = d.InfoTypeId
+    END 
+END
+GO
+
+------------------------------------------------------
+
+
+-- Concept Table:
+
+IF EXISTS (SELECT * FROM sysobjects WHERE name = 'TG_Concept_Update' AND xtype = 'TR')
+    DROP TRIGGER TG_Concept_Update
+GO
+
+CREATE TRIGGER TG_Concept_Update
+ON Concept AFTER INSERT, UPDATE
+AS
+BEGIN
+    IF UPDATE(TableName) OR UPDATE(Description) OR UPDATE(AUTO_TIME_CREATION) OR UPDATE(AUTO_TIME_UPDATE) OR UPDATE(AUTO_UPDATE_COUNT) OR UPDATE(IsDeleted) OR EXISTS (SELECT * FROM deleted)
+    BEGIN
+        UPDATE c
+        SET c.AUTO_UPDATE_COUNT = c.AUTO_UPDATE_COUNT + 1, c.AUTO_TIME_UPDATE = GETDATE()
+        FROM deleted d
+        INNER JOIN Concept c
+        ON c.ConceptId = d.ConceptId
+    END 
+END
+GO
+
+--------------------------------------------------------
+
+-- Info Table:
+
+IF EXISTS (SELECT * FROM sysobjects WHERE name = 'TG_Info_Update' AND xtype = 'TR')
+    DROP TRIGGER TG_Info_Update
+GO
+
+CREATE TRIGGER TG_Info_Update
+ON Info AFTER INSERT, UPDATE
+AS
+BEGIN
+    IF UPDATE(ConceptId) OR UPDATE(RefKeyId) OR UPDATE(InfoTypeId) OR UPDATE(Data) OR UPDATE(AUTO_TIME_CREATION) OR UPDATE(AUTO_TIME_UPDATE) OR UPDATE(AUTO_UPDATE_COUNT) OR UPDATE(IsDeleted) OR EXISTS (SELECT * FROM deleted)
+    BEGIN
+        UPDATE i
+        SET i.AUTO_UPDATE_COUNT = i.AUTO_UPDATE_COUNT + 1, i.AUTO_TIME_UPDATE = GETDATE()
+        FROM deleted d
+        INNER JOIN Info i
+        ON i.InfoId = d.InfoId
+    END 
+END
 GO
