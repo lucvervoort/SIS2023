2a08a55e50b4132fc1a625b750bfcad721a6651b
Luc Vervoort <luc.vervoort@hogent.be>
2023-10-30 16:24:08 +0100

    Demo

---
+++ SISClientDemo/ConsoleHostedService.cs
@@ -0,0 +1,65 @@
+﻿using Flurl.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using SIS.API.DTO;
+
+internal partial class Program
+{
+    internal sealed class ConsoleHostedService : IHostedService
+    {
+        private readonly ILogger _logger;
+        private readonly IConfiguration _configuration;
+        private readonly IHostApplicationLifetime _appLifetime;
+        private int? _exitCode;
+
+        public ConsoleHostedService(
+            ILogger<ConsoleHostedService> logger,
+            IConfiguration configuration,
+            IHostApplicationLifetime appLifetime)
+        {
+            _logger = logger;
+            _configuration = configuration;
+            _appLifetime = appLifetime;
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogDebug($"Starting with arguments: {string.Join(" ", Environment.GetCommandLineArgs())}");
+
+            _appLifetime.ApplicationStarted.Register(() =>
+            {
+                Task.Run(async () =>
+                {
+                    try
+                    {
+                        _logger.LogInformation("Flurl example of get and post...");
+
+                        _exitCode = 0;
+                    }
+                    catch (Exception ex)
+                    {
+                        _logger.LogError(ex, "Unhandled exception!");
+                        _exitCode = 1;
+                    }
+                    finally
+                    {
+                        // Stop the application once the work is done
+                        _appLifetime.StopApplication();
+                    }
+                });
+            });
+
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation($"Exiting with return code: {_exitCode}");
+
+            // Exit code may be null if the user cancelled via Ctrl+C/SIGTERM
+            Environment.ExitCode = _exitCode.GetValueOrDefault(-1);
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ SISClientDemo/Program.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Reflection;
+using Microsoft.Extensions.Configuration;
+using Serilog;
+using static Program;
+
+namespace SISClientDemo
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var dirName = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            // We create the generic host
+            await Host.CreateDefaultBuilder(args)
+                .UseContentRoot(dirName)
+                .ConfigureLogging(loggingBuilder =>
+                {
+                    var configuration = new ConfigurationBuilder()
+                                        .AddJsonFile("appsettings.json")
+                                        .Build();
+                    var logger = new LoggerConfiguration()
+                        .ReadFrom.Configuration(configuration)
+                        .CreateLogger();
+                    loggingBuilder.AddSerilog(logger, dispose: true);
+                })
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services
+                      .AddHostedService<ConsoleHostedService>(); // generic host integrated in console app                                                       // -------------------------------------------------------
+                })
+                .RunConsoleAsync();
+        }
+    }
+}
